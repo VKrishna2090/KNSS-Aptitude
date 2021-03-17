607da11d148623a5c7d4b9a4d3d227596972c2b3
VKrishna2090 <36009751+VKrishna2090@users.noreply.github.com>
2021-03-17 22:10:05 +0530

    Scraping of MCQs done

--- SanFoundry_Scraped.py
+++ SanFoundry_Scraped.py
@@ -2,15 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 def scrape(url):
-    # url = "https://www.sanfoundry.com/java-mcqs-integer-floating-data-types/"
-    
     q=""
     r = requests.get(url)
     soup = BeautifulSoup(r.content)
     g_data = soup.find_all("div",{"class":"entry-content"})
     for i in g_data:
         q = q + "\n" + (i.text)
-    
     return q
 
 url = input("Enter a URL:")
--- separation_quest_ans.py
+++ separation_quest_ans.py
@@ -1,34 +1,36 @@
 #---Using Regex to retrieve questions and answers
 import re
+import json
 
-hand = open('Scraped_Text.txt')
+hand = open('Scraped_Text.txt',"r")
 quest = [1,2,3,4,5,6,7]
 rows, cols = (50, 3)
 qno = dict()
 answers = dict()
-count=0
-
-for line in hand:
-    line = line.rstrip()
-    if re.search('^[1-9]\..*', line) or re.search('^[1-9][0-9]\..*', line):
-        count=count+1
 
 i=0
+
 for line in hand:
     line = line.rstrip()
-    if re.search('^[1-9]\..*', line) or re.search('^[1-9][0-9]\..*', line):
+    if re.search('^[1-9]\. .*', line) or re.search('^[1-9][0-9]\. .*', line):
         i=i+1
         ques = line
-        print(ques)
         qno[ques] = list()
         answers[ques] = ""
     if re.search('^[a-d]\).*', line):
-        print(line)
         qno[ques].append(line)
-    if re.search('^View Answer Answer:\).*', line):
-        answers[ques] = line[21,22]    
-    if i==count:
-        break
+    if re.search('^View Answer Answer:.*', line):
+        answers[ques] = line[20:22]    
 
-print(qno)
-print(answers)
+def filterTheDict(dictObj, callback):
+    newDict = dict()
+    for (key, value) in dictObj.items():
+        if callback((key, value)):
+            newDict[key] = value
+    return newDict
+
+new_qno = filterTheDict(qno, lambda elem : "following" not in elem[0])
+new_answers = filterTheDict(answers, lambda elem : "following" not in elem[0])
+
+print(json.dumps(new_qno, indent=1))
+print(json.dumps(new_answers, indent=1))
